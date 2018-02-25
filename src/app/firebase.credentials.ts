f45319e073cfd84e75590155ead0fbbe1284e165
Farhan Khoo <nws05_01091247@pb.edu.bn>
2018-02-25 13:12:52 +0800

    Added Firebase

---
+++ src/app/firebase.credentials.ts
@@ -0,0 +1,14 @@
+
+<script src="https://www.gstatic.com/firebasejs/4.10.1/firebase.js"></script>
+<script>
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBzGz-K-8U3WX9MyE6bTqOYfixiYWXxqTM",
+    authDomain: "e-sastera.firebaseapp.com",
+    databaseURL: "https://e-sastera.firebaseio.com",
+    projectId: "e-sastera",
+    storageBucket: "",
+    messagingSenderId: "673971666655"
+  };
+  firebase.initializeApp(config);
+</script>
