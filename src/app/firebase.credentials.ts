cf87e45a8b6cd5d37979324d325c8e7ca1e4a8b9
Farhan Khoo <nws05_01091247@pb.edu.bn>
2018-02-25 13:30:29 +0800

    Added Firebase 2

--- src/app/firebase.credentials.ts
+++ src/app/firebase.credentials.ts
@@ -1,14 +1,8 @@
-
-<script src="https://www.gstatic.com/firebasejs/4.10.1/firebase.js"></script>
-<script>
-  // Initialize Firebase
-  var config = {
+export const FIREBASE_CONFIG = {
     apiKey: "AIzaSyBzGz-K-8U3WX9MyE6bTqOYfixiYWXxqTM",
     authDomain: "e-sastera.firebaseapp.com",
     databaseURL: "https://e-sastera.firebaseio.com",
     projectId: "e-sastera",
-    storageBucket: "",
+    storageBucket: "e-sastera.appspot.com",
     messagingSenderId: "673971666655"
   };
-  firebase.initializeApp(config);
-</script>
