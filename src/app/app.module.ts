0190adfeca91bb9e108bcc21b4d559e4a71e07e3
Farhan Khoo <nws05_01091247@pb.edu.bn>
2018-03-01 15:05:44 +0800

    firebase removed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,26 +9,9 @@ import { ListPage } from '../pages/list/list';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { TestingPage } from '../pages/testing/testing';
-<<<<<<< HEAD
 import { HttpModule } from '@angular/http';
-import { AngularFireDatabaseModule } from 'angularfire2/database';
-import { AngularFireModule } from 'angularfire2';
-import { FirebaseProvider } from './../providers/firebase/firebase';
-
-const firebaseConfig = {
-  apiKey: "AIzaSyBzGz-K-8U3WX9MyE6bTqOYfixiYWXxqTM",
-  authDomain: "e-sastera.firebaseapp.com",
-  databaseURL: "https://e-sastera.firebaseio.com",
-  projectId: "e-sastera",
-  storageBucket: "e-sastera.appspot.com",
-  messagingSenderId: "673971666655"
 };
-=======
 import { SettingPage } from '../pages/Setting/Setting';
-import {AngularFireModule} from 'angularfire2';
-import {AngularFireDatabaseModule} from 'angularfire2/database';
-import { FIREBASE_CONFIG } from './firebase.credentials';
->>>>>>> 036158d4e771d98cd1eae5e51ff5aa5bb3b93ae6
 
 @NgModule({
   declarations: [
