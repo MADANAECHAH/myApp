677c293398ff4128a8727b68304f8e5504395554
Shafiq Ismail <nws05_01090920@pb.edu.bn>
2018-03-04 12:09:42 +0800

    Add alert on Login button

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -3,6 +3,7 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { User } from '../../models/user';
 import { AngularFireAuth } from 'angularfire2/auth';
 import { HomePage } from '../home/home';
+import { AlertController } from 'ionic-angular';
 
 /**
  * Generated class for the LoginPage page.
@@ -20,20 +21,38 @@ export class LoginPage {
 
   user = {} as User;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, private afAuth: AngularFireAuth) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, private afAuth: AngularFireAuth, public alerCtrl: AlertController) {
   }
+
+  doAlert(){
+  let alert = this.alerCtrl.create({
+    title: 'Welcome!',
+    message : 'You Are Logged In ',
+    buttons : ['OK']
+  });
+alert.present()
+  }
+
+  doAlert1(){
+    let alert = this.alerCtrl.create({
+      title: 'Not Logged In',
+      message : 'Wrong Password Or Email Please Check Again. ',
+      buttons: ['OK']
+    });
+  alert.present()
+    }
 
   async login(user : User) {
      try{
        const result = await this.afAuth.auth.signInAndRetrieveDataWithEmailAndPassword(user.email, user.password);
        console.log(result);
        if(result){
-         this.navCtrl.setRoot('HomePage');
+         this.doAlert();
        }
      }
      catch (e){
        console.error(e);
-     }
+       this.doAlert1();     }
    }
 
 register() {
